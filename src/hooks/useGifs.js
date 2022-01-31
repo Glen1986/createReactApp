93540e082040a03ea132b7f200a092d8bfbfecd0
Glen1986 <escobar.glen@gmail.com>
2022-01-31 17:19:32 -0300

    LazyLoadPag

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -2,16 +2,19 @@ import {useState, useEffect, useContext} from 'react'
 import getGifs from '../services/getGifs'
 import GifsContext from '../context/GifsContext'
 
+const INITIAL_PAGE = 0
 
 export function useGifs({keyword}){
   const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(INITIAL_PAGE)
+  const [loadingNextPage, setLoadingNextPage] = useState(false)
   const {gifs, setGifs} = useContext(GifsContext)
 // const [gifs, setGifs] = useState([])
 
+  const keywordToUse = keyword ||localStorage.getItem('lastKeyword') || 'random'
   useEffect(function () {
     setLoading(true)
 // recuperamos keyword de local.storage
-   const keywordToUse = keyword ||localStorage.getItem('lastKeyword') || 'random'
     getGifs({ keyword: keywordToUse})
       .then(gifs => {
         setGifs(gifs)
@@ -20,5 +23,12 @@ export function useGifs({keyword}){
         localStorage.setItem('lastKeyword', keyword)
       })
   }, [keyword, setGifs])
-return {loading, gifs}
+   useEffect(()=>{
+     if(page === INITIAL_PAGE ) return
+getGifs({keyword:keywordToUse, page})
+.then(nextGifs=>{
+setGifs(prevGifs => prevGifs.concat(nextGifs)) 
+})
+},[page])
+return {loading, loadingNextPage, gifs, setPage}
 }
